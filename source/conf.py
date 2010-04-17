da9aa8aa62d6c4a66e8c41c67441caff1ba20fda
Michael Markert <markert.michael@gmail.com>
2010-04-18 00:31:13 +0200

    Updated copyright.

--- source/conf.py
+++ source/conf.py
@@ -38,7 +38,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Python Tutorial'
-copyright = u'2009, Michael Markert et al'
+copyright = u'2010, Michael Markert et al'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
