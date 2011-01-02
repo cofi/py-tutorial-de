c017ce4803a1515b1694160e5144f4b2187c236b
Michael Markert <markert.michael@gmail.com>
2011-01-02 16:41:56 +0100

    Build PDF for all branches.

--- mk-pdf.sh
+++ mk-pdf.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 dir=$(dirname $(readlink -f $0))
 latexdir=${dir}/build/latex
-branch=$(hg branch)
+pbranch=$(hg branch)
 
-make latex
-cd ${latexdir}
-make all-pdf
-cd ${dir}
-
-date=$(date +%Y-%m-%d)
-mv ${latexdir}/python-tutorial.pdf ${dir}/tutorial-${branch}-${date}.pdf
+for branch in "python-3.1" "python-3.2"; do
+    hg update ${branch}
+    make latex
+    cd ${latexdir}
+    make all-pdf
+    cd ${dir}
+    date=$(date +%Y-%m-%d)
+    mv ${latexdir}/python-tutorial.pdf ${dir}/tutorial-${branch}-${date}.pdf
+done
+hg update ${pbranch}
