58d794ce7e3b065bcb2822adad5d69dc804870bf
Michael Markert <markert.michael@gmail.com>
2012-10-31 22:44:54 +0100

    Bump copyright notice to 2012.

--- source/conf.py
+++ source/conf.py
@@ -38,7 +38,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Python Tutorial'
-copyright = u'2010 – 2011, Michael Markert et al'
+copyright = u'2010 – 2012, Michael Markert et al'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
